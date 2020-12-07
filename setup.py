8884d2ad5416d4612b74139db580940cbc1054da
Jakub Sokołowski <jakub@status.im>
2020-12-07 21:39:54 +0100

    add setup.py and setup.cfg
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(name='cabot_check_status_go',
+      version='0.1.0',
+      description='A status-go canary check plugin for Cabot by Arachnys',
+      author='Jakub Sokołowski',
+      author_email='jakub@status.im',
+      url='https://github.com/status-im/cabot-check-status-go',
+      packages=find_packages(),
+      download_url = 'https://github.com/status-im/cabot-check-status-go/tarball/0.1.0',
+    )
